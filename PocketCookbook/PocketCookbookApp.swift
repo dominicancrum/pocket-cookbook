e2e7f087ba507d14146b4fc01e0e4e507972b131
DJ Ancrum <dominicancrum@gmail.com>
2025-02-06 01:42:52 -0500

    Initial Commit

---
+++ PocketCookbook/PocketCookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketCookbookApp.swift
+//  PocketCookbook
+//
+//  Created by Dominic Ancrum on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PocketCookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PocketCookbookTests/PocketCookbookTests.swift
@@ -0,0 +1,17 @@
+//
+//  PocketCookbookTests.swift
+//  PocketCookbookTests
+//
+//  Created by Dominic Ancrum on 2/6/25.
+//
+
+import Testing
+@testable import PocketCookbook
+
+struct PocketCookbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
